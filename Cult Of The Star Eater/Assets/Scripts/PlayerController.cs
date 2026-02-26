b3e40cc4603bcec93c0f967e43f028b2f994348d
dhd1003 <119531097+dhd1003@users.noreply.github.com>
2026-02-26 01:41:50 +0100

    Primeras implementaciones Character Controlller

--- Cult Of The Star Eater/Assets/Scripts/PlayerController.cs
+++ Cult Of The Star Eater/Assets/Scripts/PlayerController.cs
@@ -1,121 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody))]
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movimiento")]
-    public float moveSpeed = 8f;
+    CharacterController characterController;
 
-    [Header("Salto")]
-    public float jumpForce = 7f;
-    public int maxJumps = 2;
+    Animator animator;
 
-    [Header("Dash")]
-    public float dashSpeed = 20f;
-    public float dashDuration = 0.2f;
-    public float dashCooldown = 0.5f;
+    public float speed = 6f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 1f;
 
-    [Header("Ground Check")]
-    public Transform groundCheck;
-    public float groundDistance = 0.3f;
-    public LayerMask groundLayer;
+    private float horizontalMove;
+    private float verticalMove;
 
-    private Rigidbody rb;
-    private int jumpsRemaining;
-    private bool isGrounded;
-    private bool isDashing;
-    private bool canDash = true;
+    //private Vector3 moveDirection = Vector3.zero;//Preguntar JONE
+    
 
-    private float moveInput;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
-        // Congelar rotaciones para estilo 2D
-        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        CheckGround();
 
-        if (!isDashing)
+        horizontalMove = Input.GetAxis("Horizontal");
+        
+        if (Input.GetButtonDown("Jump"))
         {
-            moveInput = Input.GetAxisRaw("Horizontal");
+            Debug.Log("Saltando");
         }
+        
+        
+        //if (characterController.isGrounded) 
+        //{
+        //    animator.SetBool("IsJumping", false);
 
-        HandleJump();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            TryDash();
-        }
+        //}
+
+
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (!isDashing)
+        // Apply gravity
+        characterController.Move(new Vector3(0, -gravity, 0) * Time.deltaTime);
+
+
+        if (characterController.isGrounded)
         {
-            rb.velocity = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0f);
-        }
-    }
+            if (Input.GetButton("Jump"))
+            {
+                characterController.Move(new Vector3(0, jumpSpeed, 0) * Time.deltaTime);
 
-    void HandleJump()
-    {
-        if (isGrounded)
-        {
-            jumpsRemaining = maxJumps;
+            }
         }
 
-        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-            jumpsRemaining--;
-        }
-    }
-
-    void CheckGround()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
-    }
-
-    void TryDash()
-    {
-        if (canDash)
-        {
-            StartCoroutine(Dash());
-        }
-    }
-
-    IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-
-        float originalGravity = rb.useGravity ? Physics.gravity.y : 0f;
-
-        rb.velocity = new Vector3(moveInput == 0 ? transform.localScale.x : moveInput, 0f, 0f).normalized * dashSpeed;
-
-        yield return new WaitForSeconds(dashDuration);
-
-        isDashing = false;
-
-        yield return new WaitForSeconds(dashCooldown);
-
-        canDash = true;
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
-        }
+        characterController.Move(new Vector3(horizontalMove,0, 0) * speed * Time.deltaTime);
     }
 }
